d0bfb77ad71818f06d61bf73894899a2bbc45816
mijan-joy <mmrsm46@gmail.com>
2022-03-01 21:45:31 +0600

    loading and error ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
 const searchPhone = () => {
+  document.getElementById("search-result").textContent = "";
+  document.getElementById("phone-details").textContent = "";
+
   const searchValue = document.getElementById("phone-input").value;
-  //   console.log(searchValue);
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-  //   console.log(url);
-  fetch(url)
-    .then((Response) => Response.json())
-    .then((data) => displayResult(data.data.slice(0, 9)));
-  document.getElementById("phone-input").value = "";
+
+  if (searchValue == "") {
+    displayError("block");
+  } else if (searchResult() = null) {
+    notfound("block");
+    console.log(notfound);
+  } else {
+    displayError("none");
+    displaySpinner("block");
+    //   console.log(searchValue);
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    //   console.log(url);
+    fetch(url)
+      .then((Response) => Response.json())
+      .then((data) => displayResult(data.data.slice(0, 20)));
+    // document.getElementById("phone-input")= "";
+  }
 };
+
 const displayResult = (brands) => {
+  displaySpinner("none");
   //   console.log(phones);
   for (const brand of brands) {
     console.log(brand);
+
     const searchResult = document.getElementById("search-result");
     const div = document.createElement("div");
     div.innerHTML = `
@@ -22,8 +38,13 @@ const displayResult = (brands) => {
             <h6 class="card-title  text-center">${brand.brand}</h6>
             <h6 class="card-title  text-center">Model: ${brand.slug}</h6>
             <p class="card-text"> </p>
-            <button onclick="loadDetails('${brand.slug}')" class="btn btn-success btn-sm">Details</button>
+           
+            <div class="mx-auto text-center"><button onclick="loadDetails('${brand.slug}')" type="button" class="btn btn-dark btn-sm ">See More</button>
             </div>
+            
+            
+            </div>
+            
         </div>
     `;
     searchResult.appendChild(div);
@@ -45,15 +66,95 @@ const setDetails = (slug) => {
   const div = document.createElement("div");
 
   div.innerHTML = `
-  <img class="card-img-top" src="${slug.image}" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">${slug.slug}</h5>
-          <p class="card-text">${slug.releaseDate}</p>
-          <p class="card-text">${slug.mainFeatures.storage}</p>
-          <p class="card-text">Sensor: ${slug.mainFeatures.sensors}</p>
-          
-          <a href="${slug.strYoutube}" class="btn btn-primary">Go somewhere</a>,
+  <div class="card mb-3 mx-auto col-md-9 p-1 shadow-lg">
+  <div class="row g-0">
+    <div class="col-md-4 my-auto">
+      <img src="${slug.image}" class="img-fluid rounded-start w-100" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title text-uppercase">${slug.slug}</h4>
+
+        <div class="accordion" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+          <button
+            class="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            <h6>Main Features</h6>
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse show"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <strong>Chip Set:</strong><code> ${slug.mainFeatures.chipSet}</code><br />
+            <strong>SisplaySize:</strong><code> ${slug.mainFeatures.displaySize}</code><br />
+            <strong>Storage:</strong><code> ${slug.mainFeatures.storage}</code><br />
+            <strong>Memory:</strong><code> ${slug.mainFeatures.memory}</code><br />
+            <strong>Brand:</strong><code> ${slug.brand}</code><br />
+            
+          </div>
         </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            <h6>Sensors & Others Features</h6>
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          class="accordion-collapse collapse"
+          aria-labelledby="headingTwo"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body text-center">
+            <strong>Sensors:</strong><code><br/> ${slug.mainFeatures.sensors}</code><br />
+            <strong>Others Features :</strong><br />
+            <strong>W-Lan:</strong><code> ${slug.others.WLAN}</code><br />
+            <strong>Bluetooth:</strong><code> ${slug.others.Bluetooth}</code><br />
+            <strong>GPS:</strong><code> ${slug.others.GPS}</code><br />
+            <strong>NFC:</strong><code> ${slug.others.NFC}</code><br />
+            <strong>Radio:</strong><code> ${slug.others.Radio}</code><br />
+            <strong>USB:</strong><code> ${slug.others.USB}</code>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
+      </div>
+      
+    </div>
+  </div>
+</div>
   `;
   phoneDetails.append(div);
+};
+const displaySpinner = (condition) => {
+  document.getElementById("spinner").style.display = condition;
+  // console.log(displaySpinner);
+};
+
+const displayError = (condition) => {
+  document.getElementById("error").style.display = condition;
+  // console.log(displayError);
+};
+const notfound = (condition) => {
+  document.getElementById("notfound").style.display = condition;
 };
